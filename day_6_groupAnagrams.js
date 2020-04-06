4cad331e58256b46271962d48bf732361e399900
Jacob Moss <jmoss@indeed.com>
2020-04-06 18:39:29 -0500

    hashmap with each strings letter counts is built as expected

--- day_6_groupAnagrams.js
+++ day_6_groupAnagrams.js
@@ -15,5 +15,23 @@
 // The order of your output does not matter.
 
 var groupAnagrams = function(strs) {
-    
+    //create dict with counts of letters in each string
+    //if other dict values match the first, then create array with those strings for the return
+    const my_dict = {};
+    // let lettersObject = {}, currentString;
+    for (let i = 0; i < strs.length; i++) {
+        let lettersObject = {};
+        // currentString = strs[i];
+        for (let j = 0; j < strs[i].length; j++) {
+            (lettersObject[strs[i][j]]) ? lettersObject[strs[i][j]]++: lettersObject[strs[i][j]] = 1;
+            // my_dict[strs[i]] = lettersObject;
+            // console.log(lettersObject)
+        }
+        my_dict[strs[i]] = lettersObject;
+        // my_dict[strs[i]] = lettersObject;
+    }
+    console.log(my_dict)
+
 };
+
+groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
