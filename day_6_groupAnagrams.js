3bf574603f1d7745b5e54549a7d59f567914d104
Jacob Moss <jmoss@indeed.com>
2020-04-06 19:11:15 -0500

    idea behind pushing anagrams to final result array solidified. Need to figure out how to identify anagrams appropriately

--- day_6_groupAnagrams.js
+++ day_6_groupAnagrams.js
@@ -17,21 +17,32 @@
 var groupAnagrams = function(strs) {
     //create dict with counts of letters in each string
     //if other dict values match the first, then create array with those strings for the return
-    const my_dict = {};
-    // let lettersObject = {}, currentString;
+    //first see if the two words have all of the same letters
+    //then see if they have the same amount of those letters
+    const my_dict = {}, resultArrays = [];
+
+
     for (let i = 0; i < strs.length; i++) {
         let lettersObject = {};
-        // currentString = strs[i];
         for (let j = 0; j < strs[i].length; j++) {
             (lettersObject[strs[i][j]]) ? lettersObject[strs[i][j]]++: lettersObject[strs[i][j]] = 1;
-            // my_dict[strs[i]] = lettersObject;
-            // console.log(lettersObject)
         }
         my_dict[strs[i]] = lettersObject;
-        // my_dict[strs[i]] = lettersObject;
     }
-    console.log(my_dict)
+    // console.log(my_dict)
+    for (let k = 0; k < Object.keys(my_dict).length; k++) {
+        let currentValues = my_dict[Object.keys(my_dict)[k]]
+        let anagramArray = [];
+        anagramArray.push(Object.keys(my_dict)[k])
+        for (let l = 0; l < Object.keys(my_dict).length; l++) {
+            if (my_dict[Object.keys(my_dict)[k]] === my_dict[Object.keys(my_dict)[l]]) {
+                anagramArray.push(Object.keys(my_dict)[l])
+            }
+        }
+        resultArrays.push(anagramArray);
+    }
 
+    console.log(resultArrays);
 };
 
 groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]);
